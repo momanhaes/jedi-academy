027237cc24943ecc56c07666b9745d765128c9dd
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-31 12:47:02 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Student } from "./student/students.model";
 
 @Component({
-  selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "jad-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  students: Student[] = [
+    { name: "Luke", isJedi: true, temple: "Coruscant" },
+    { name: "Leia", isJedi: false },
+    { name: "Han Solo", isJedi: false },
+  ];
 }
